42eeea69bb729073aaa550b5184d36ab02485ff7
jelf <353742991@qq.com>
2018-08-14 09:56:03 +0800

    style: 修改主题色并新增global.less存放全局样式

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,26 +6,36 @@ module.exports = {
     head: {
         title: 'cat-girl',
         meta: [
-            {charset: 'utf-8'},
-            {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-            {hid: 'description', name: 'description', content: 'cat-girl'}
+            { charset: 'utf-8' },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1'
+            },
+            { hid: 'description', name: 'description', content: 'cat-girl' }
         ],
         link: [
-            {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
-            {rel: 'stylesheet', href: 'https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css'},
-            {rel: 'stylesheet', href: '//at.alicdn.com/t/font_780221_m5cpvrpue.css'}
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+            {
+                rel: 'stylesheet',
+                href:
+                    'https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css'
+            },
+            {
+                rel: 'stylesheet',
+                href: '//at.alicdn.com/t/font_780221_m5cpvrpue.css'
+            }
         ]
     },
     /*
     ** Customize the progress bar color
     */
-    loading: {color: '#df3b85'},
+    loading: { color: '#D26B9E' },
     router: {
         linkActiveClass: 'router-link-active',
         linkExactActiveClass: 'router-link-exact-active'
     },
     css: [
-        './assets/styles/base.less',
+        './assets/styles/global.less',
         './assets/styles/swiper-3.4.2.min.css'
         // './assets/styles/normalize.css'
     ],
@@ -37,7 +47,7 @@ module.exports = {
         /*
         ** Run ESLint on save
         */
-        extend(config, {isDev, isClient}) {
+        extend(config, { isDev, isClient }) {
             if (isDev && isClient) {
                 config.module.rules.push({
                     enforce: 'pre',
@@ -49,28 +59,19 @@ module.exports = {
         }
     },
     plugins: [
-        {src: '~/plugins/swiper.js', ssr: false},
+        { src: '~/plugins/swiper.js', ssr: false }
     ],
-    modules: [
-        '@nuxtjs/axios'
-    ],
+    modules: ['@nuxtjs/axios'],
     axios: {
         timeout: 10000,
         headers: {},
         interceptors: {
             request: {
-                use: [
-                    config => config,
-                    err => Promise.reject(err)
-                ]
+                use: [config => config, err => Promise.reject(err)]
             },
             response: {
-                use: [
-                    response => response,
-                    err => Promise.reject(err)
-                ]
+                use: [response => response, err => Promise.reject(err)]
             }
         }
     }
 }
-
