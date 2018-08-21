507ee93304f5cda54732a3944359758eb5866862
jelf <353742991@qq.com>
2018-08-21 22:14:18 +0800

    feat: 新增个人中心首页

---
+++ components/UserInfo/index.vue
@@ -0,0 +1,64 @@
+<template>
+    <div :class="prefixCls">
+        <img class="avatar" src="https://static.hdslb.com/images/member/noface.gif">
+        <div class="user-profile">
+            <h2 class="user-profile-username">木偶猪猪</h2>
+            <p><var>1001</var>天 <var>20000</var>金币</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'user-info',
+        props: {
+            prefixCls: {
+                type: String,
+                default: 'user-info'
+            }
+        }
+    }
+</script>
+
+<style lang="less">
+    @import "../../assets/styles/base";
+
+    @prefixCls: user-info;
+    .@{prefixCls} {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        .avatar {
+            width: 60px;
+            border-radius: 50%;
+            border: 2px solid #fff;
+            overflow: hidden;
+            margin-right: 10px;
+        }
+        .user-profile {
+            &-username {
+                font-size: 16px;
+            }
+            p {
+                color: rgba(0, 0, 0, .7);
+            }
+            var {
+                color: @primary-color;
+            }
+        }
+    }
+
+    @media (min-width: @screen-md-min) {
+        .@{prefixCls} {
+            .avatar {
+                width: 80px;
+                margin-right: 20px;
+            }
+            .user-profile {
+                &-username {
+                    font-size: 20px;
+                }
+            }
+        }
+    }
+</style>
---
+++ pages/userCenter/index.vue
@@ -0,0 +1,80 @@
+<template>
+    <div :class="prefixCls">
+        <user-info></user-info>
+        <ul :class="`${prefixCls}-menu`">
+            <li>
+                <span>充值中心</span>
+                <nuxt-link to="/">进入查看 >></nuxt-link>
+            </li>
+            <li>
+                <span>充值记录</span>
+                <nuxt-link to="/">进入查看 >></nuxt-link>
+            </li>
+            <li>
+                <span>购买记录</span>
+                <nuxt-link to="/">进入查看 >></nuxt-link>
+            </li>
+            <li>
+                <span>会员资料</span>
+                <nuxt-link to="/">修改 >></nuxt-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import UserInfo from '../../components/UserInfo/index'
+
+    export default {
+        name: 'user-center',
+        components: { UserInfo },
+        props: {
+            prefixCls: {
+                type: String,
+                default: 'pg-user-center'
+            }
+        }
+    }
+</script>
+
+<style lang="less">
+    @import "../../assets/styles/base";
+
+    @prefixCls: pg-user-center;
+    .@{prefixCls} {
+        padding: 20px;
+        &-menu {
+            display: flex;
+            align-items: center;
+            flex-flow: column;
+            padding: 0;
+            margin: 20px 0 0 0;
+            list-style-type: none;
+            li {
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+                padding: 0 10px;
+                width: 90%;
+                height: 40px;
+                border-top: 1px solid #ddd;
+                color: #999;
+                &:last-child {
+                    border-bottom: 1px solid #ddd;
+                }
+            }
+        }
+    }
+
+    @media (min-width: @screen-md-min) {
+        .@{prefixCls} {
+            &-menu {
+                margin: 40px 0 0 0;
+                li {
+                    width: 60%;
+                    height: 50px;
+                }
+            }
+        }
+    }
+</style>
