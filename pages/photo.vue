5c729fac674d5ed47ca9762089b6422e795f62a7
jelf <353742991@qq.com>
2018-09-03 22:40:37 +0800

    feat: 新增写真页面

---
+++ pages/photo.vue
@@ -0,0 +1,98 @@
+<template>
+    <div :class="prefixCls">
+        <order style="display: flex;justify-content: flex-end" :order="order"/>
+        <photo-list>
+            <photo-list-item v-for="item in fakerData"
+                             :data="item"
+                             :key="item.id">
+                <footer-default slot="footer"/>
+            </photo-list-item>
+        </photo-list>
+    </div>
+</template>
+
+<script>
+    import { FooterDefault, PhotoList, PhotoListItem } from '../components/PhotoList'
+    import Order                                       from '../components/Order'
+
+    export default {
+        name: 'photo',
+        components: { Order, PhotoListItem, PhotoList, FooterDefault },
+        props: {
+            prefixCls: {
+                type: String,
+                default: 'pg-photo'
+            }
+        },
+        data() {
+            return {
+                order: {
+                    isShow: false,
+                    orderField: '',
+                    data: [
+                        {
+                            field: '',
+                            text: '最新'
+                        },
+                        {
+                            field: 'hot',
+                            text: '最热'
+                        },
+                        {
+                            field: 'recommend',
+                            text: '推荐'
+                        }
+                    ]
+                },
+                fakerData: [ {
+                    id: 1,
+                    photo: '/sucai.png',
+                    contract: 'Jelf'
+                }, {
+                    id: 2,
+                    photo: '/sucai.png',
+                    contract: 'Jelf1'
+                }, {
+                    id: 3,
+                    photo: '/sucai.png',
+                    contract: 'Jel2'
+                }, {
+                    id: 4,
+                    photo: '/sucai.png',
+                    contract: '唐伯虎点秋香'
+                }, {
+                    id: 5,
+                    photo: '/sucai.png',
+                    contract: '唐伯虎点秋香'
+                }, {
+                    id: 6,
+                    photo: '/sucai.png',
+                    contract: 'zoe'
+                }, {
+                    id: 7,
+                    photo: '/sucai.png',
+                    contract: '唐伯虎点秋香'
+                }, {
+                    id: 8,
+                    photo: '/sucai.png',
+                    contract: '唐伯虎点秋香'
+                } ]
+            }
+        }
+    }
+</script>
+
+<style lang="less">
+    @import "../assets/styles/base";
+
+    @prefixCls: pg-photo;
+    .@{prefixCls} {
+        padding: 20px;
+
+    }
+
+    @media (min-width: @screen-md-min) {
+        .@{prefixCls} {
+        }
+    }
+</style>
